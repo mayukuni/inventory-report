f235b2ac6dabeaa0344eb8ef6daaad42574445d8
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2023-01-18 06:51:34 -0300

    req 3

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,21 @@
+from inventory_report.reports.simple_report import SimpleReport
+from collections import Counter
+
+
 class CompleteReport:
-    pass
+    @staticmethod
+    def generate(list):
+        simple_report = SimpleReport.generate(list)
+        
+        products = Counter(item['nome_da_empresa'] for item in list)
+        
+        stock_products = ''
+        for company in products:
+            stock_products += f"{company}: {products[company]}\n"
+
+        return (
+            f"{simple_report}\n"
+            f"Produtos estocados por empresa:\n"
+            f"{stock_products}"
+        )
+        
