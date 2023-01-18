bc308db0769bb8f148e860b295b77c90c54d507b
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2023-01-18 06:25:40 -0300

    req 2

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,22 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(products: list):
+        fabrication_date = min(
+            [product["data_de_fabricacao"] for product in products]
+        )
+
+        today = datetime.now().isoformat()
+
+        valid_date = min([product["data_de_validade"] for product in products if product["data_de_validade"] > today])
+        
+        company, _ = Counter([product["nome_da_empresa"] for product in products]).most_common()[0]
+
+        return (
+            f"Data de fabricação mais antiga: {fabrication_date}\n"
+            f"Data de validade mais próxima: {valid_date}\n"
+            f"Empresa com mais produtos: {company}"
+        )
