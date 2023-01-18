9c30cbeaf148031dd26bd61e28ebb6c107a4f441
Mayumi <cinthya.m.kuniyoshi@gmail.com>
2023-01-18 08:45:15 -0300

    req 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,1 +1,16 @@
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+import csv
 
+
+class Inventory:
+    @staticmethod
+    def import_data(path, type):
+        data = []
+        if "csv" in path:
+            with open(path, encoding="utf-8") as file:
+                data = list(csv.DictReader(file))
+
+        if type == 'simples':
+            return SimpleReport.generate(data)
+        return CompleteReport.generate(data)
